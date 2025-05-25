6ff5dda6b1b053a50e9e1b12aad461a591f57873
zidane.nur004 <zidane.nur004@gmail.com>
2025-05-25 01:14:52 +0000

    little fix

--- src/ui/QuizContent.tsx
+++ src/ui/QuizContent.tsx
@@ -7,7 +7,7 @@ import { useContext, useEffect, useState } from "react";
 export default function QuizContent() {
     const { quiz } = useContext(quizContext);
 
-    const [selectedQuiz, setSelectedQuiz] = useState<Quiz>(quiz[0]);
+    const [selectedQuiz, setSelectedQuiz] = useState<Quiz | null>(null);
     const [answers, setAnswers] = useState<number[]>([]);
     const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
 
@@ -18,15 +18,17 @@ export default function QuizContent() {
             const answersArr: number[] = JSON.parse(storedAnswers);
             setAnswers(answersArr);
 
-            setSelectedQuiz(quiz[answers.length]);
         }
+        setSelectedQuiz(quiz[answers.length]);
     }, []);
 
     useEffect(() => {
         console.log(selectedAnswer);
     }, [selectedAnswer]);
 
-    return (
+    return !selectedQuiz ? (
+        <div className="flex justify-center items-center w-full">Loading....</div>
+    ) : (
         <main className="flex flex-col items-center w-full md:px-64 py-10 h-full gap-y-8">
             <div className="flex justify-between w-full">
                 <h2 className="text-2xl font-bold text-blue-400">Quiz Master</h2>
